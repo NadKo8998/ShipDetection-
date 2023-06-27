4f4775bae7fbe9e8581d5e4b34312a14139f993c
NadKo8998 <137818457+NadKo8998@users.noreply.github.com>
2023-06-27 22:12:13 +0300

    Update train.py

--- train.py
+++ train.py
@@ -70,150 +70,75 @@ train_x, train_y = next(train_gen)
 valid_x, valid_y = next(dataGenerator(valid_value, count))
 
 
-#2.3 Data Augmentation
-dg_args = dict(featurewise_center = False, 
-                  samplewise_center = False,
-                  rotation_range = 45, 
-                  width_shift_range = 0.1, 
-                  height_shift_range = 0.1, 
-                  shear_range = 0.01,
-                  zoom_range = [0.9, 1.25],  
-                  horizontal_flip = True, 
-                  vertical_flip = True,
-                  fill_mode = 'reflect',
-                   data_format = 'channels_last')
-
-
-if AUGMENT_BRIGHTNESS:
-    dg_args[' brightness_range'] = [0.5, 1.5]
-image_gen = ImageDataGenerator(**dg_args)
-
-if AUGMENT_BRIGHTNESS:
-    dg_args.pop('brightness_range')
-label_gen = ImageDataGenerator(**dg_args)
- 
-  
-cur_gen = generators.create_aug_gen(train_gen)
-t_x, t_y = next(cur_gen)
-
-import gc; gc.enable() 
-gc.collect()
-
-#3-1. Base Model
-def upsample_conv(filters, kernel_size, strides, padding):
-    return layers.Conv2DTranspose(filters, kernel_size, strides=strides, padding=padding)
-def upsample_simple(filters, kernel_size, strides, padding):
-    return layers.UpSampling2D(strides)
-
-if UPSAMPLE_MODE=='DECONV':
-    upsample=upsample_conv
-else:
-    upsample=upsample_simple
-    
+########################################################################################################################
+# Model definition
+########################################################################################################################
 def unet(pretrained_weights = None, input_size = (256, 256, 3), NET_SCALING = NET_SCALING):
     inputs = layers.Input(input_size)
 
-    c1 = layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
-    c1 = layers.Dropout(0.1)(c1)
-    c1 = layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
-    p1 = layers.MaxPooling2D((2, 2))(c1)
+     conv1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    c2 = layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
-    c2 = layers.Dropout(0.1)(c2)
-    c2 = layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
-    p2 = layers.MaxPooling2D((2, 2))(c2)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    c3 = layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
-    c3 = layers.Dropout(0.2)(c3)
-    c3 = layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
-    p3 = layers.MaxPooling2D((2, 2))(c3)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-    c4 = layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
-    c4 = layers.Dropout(0.2)(c4)
-    c4 = layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
-    p4 = layers.MaxPooling2D(pool_size=(2, 2))(c4)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same')(conv4)
+    drop4 = tf.keras.layers.Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
-    c5 = layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
-    c5 = layers.Dropout(0.3)(c5)
-    c5 = layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    # Decoding Path
+    up5 = Conv2D(256, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(pool4))
+    merge5 = concatenate([drop4, up5], axis=3)
+    conv5 = Conv2D(256, 3, activation='relu', padding='same')(merge5)
+    conv5 = Conv2D(256, 3, activation='relu', padding='same')(conv5)
 
-    u6 = layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
-    u6 = layers.concatenate([u6, c4])
-    c6 = layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
-    c6 = layers.Dropout(0.2)(c6)
-    c6 = layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+    up6 = Conv2D(128, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv5))
+    merge6 = concatenate([conv3, up6], axis=3)
+    conv6 = Conv2D(128, 3, activation='relu', padding='same')(merge6)
+    conv6 = Conv2D(128, 3, activation='relu', padding='same')(conv6)
 
-    u7 = layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
-    u7 = layers.concatenate([u7, c3])
-    c7 = layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
-    c7 = layers.Dropout(0.2)(c7)
-    c7 = layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+    up7 = Conv2D(64, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv6))
+    merge7 = concatenate([conv2, up7], axis=3)
+    conv7 = Conv2D(64, 3, activation='relu', padding='same')(merge7)
+    conv7 = Conv2D(64, 3, activation='relu', padding='same')(conv7)
 
-    u8 = layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
-    u8 = layers.concatenate([u8, c2])
-    c8 = layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
-    c8 = layers.Dropout(0.1)(c8)
-    c8 = layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+    up8 = Conv2D(32, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv7))
+    merge8 = concatenate([conv1, up8], axis=3)
+    conv8 = Conv2D(32, 3, activation='relu', padding='same')(merge8)
+    conv8 = Conv2D(32, 3, activation='relu', padding='same')(conv8)
+    conv8 = Conv2D(2, 3, activation='relu', padding='same')(conv8)
 
-    u9 = layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
-    u9 = layers.concatenate([u9, c1], axis=3)
-    c9 = layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
-    c9 = layers.Dropout(0.1)(c9)
-    c9 = layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+    # Output Layer
+    output = Conv2D(1, 1, activation='sigmoid')(conv8)
 
-    d = layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
-    
-    seg_model = models.Model(inputs=[inputs], outputs=[d])
-    #seg_model.summary()
-    
-    if(pretrained_weights):
-        model.load_weights(pretrained_weights)
-        
-    return seg_model
+    # Define the model
+    model = Model(inputs=inputs, outputs=output)
 
-seg_model = unet()
-
-#3-2. callbacks setting
-weight_path="fullres_model & weights/{}_weights.best.hdf5".format('seg_model')
-
-checkpoint = ModelCheckpoint(weight_path, monitor='val_dice_coef', verbose=1, 
-                             save_best_only=True, mode='max', save_weights_only = True)
-
-reduceLROnPlat = ReduceLROnPlateau(monitor='val_dice_coef', factor=0.5, 
-                                   patience=3, 
-                                   verbose=1, mode='max', epsilon=0.0001, cooldown=2, min_lr=1e-6)
-early = EarlyStopping(monitor="val_dice_coef", 
-                      mode="max", 
-                      patience=15)
-callbacks_list = [checkpoint, early, reduceLROnPlat]
+    return model
 
 
-#3-4. Training
-def fit():
-    seg_model.compile(optimizer=Adam(1e-3, decay=1e-6), loss=losses.FocalLoss, metrics=[losses.dice_coef, 'binary_accuracy'])
-    
-    step_count = min(MAX_TRAIN_STEPS, train_df.shape[0]//BATCH_SIZE)
-    aug_gen = generators.create_aug_gen(generators.make_image_gen(train_df))
-    loss_history = [seg_model.fit(aug_gen,
-                                 steps_per_epoch=step_count,
-                                 epochs=MAX_TRAIN_EPOCHS,
-                                 validation_data=(valid_x, valid_y),
-                                 callbacks=callbacks_list,
-                                workers=1 # the generator is not very thread safe
-                                           )]
-    return loss_history
-  
+# Train the network
+########################################################################################################################
+early_stopping = EarlyStopping(patience=50, verbose=1)
+checkpoint = ModelCheckpoint(filepath='model.hdf5', save_best_only=True, verbose=1)
+csv_logger = CSVLogger('Training log.csv', separator=';',append=True)
+logger = LambdaCallback(on_epoch_end=lambda epoch, logs: f.write(str(epoch) +'\t'
+                                                                 + str(logs['loss']) +'\t'
+                                                                 + str(logs['val_loss']) + '\t'
+                                                                 + '\n'),
+                        on_train_end=lambda logs: f.close())
 
-while True:
-    loss_history = fit()
-    if np.min([mh.history['val_dice_coef'] for mh in loss_history]) < -0.2:
-        break
-        
-if IMG_SCALING is not None:
-    fullres_model = models.Sequential()
-    fullres_model.add(layers.AvgPool2D(IMG_SCALING, input_shape = (None, None, 3)))
-    fullres_model.add(seg_model)
-    fullres_model.add(layers.UpSampling2D(IMG_SCALING))
-else:
-    fullres_model = seg_model
-fullres_model.save('fullres_model & weights/fullres_model.h5')
+history = model.fit_generator(generator=training_generator,
+                              steps_per_epoch=250,
+                              epochs=500,
+                              validation_data=validation_generator,
+                              validation_steps=len(validation_generator),
+                              callbacks=[checkpoint, logger, csv_logger],
+                              verbose=1)
